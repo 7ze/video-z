82985abf56fbd61a5e0b5b14dc836bbd7003840b
The-sad-philosopher <shinudonney@tuta.io>
2020-12-29 09:37:47 +0530

    Fix youtube api 💎

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react';
 import './App.sass';
 import { SearchBar } from '../SearchBar';
+import { youtube } from '../../api';
 
 type State = {};
 
 type Props = {};
+
 export class App extends React.Component<Props, State> {
-  onFormSubmit = (searchTerm: string) => {
-    console.log(searchTerm);
+  onFormSubmit = async (searchTerm: string) => {
+    try {
+      const response = await youtube.get('/search', {
+        params: { q: searchTerm },
+      });
+      console.log(response);
+    } catch (e) {
+      console.log(e.toJSON());
+    }
   };
 
   render() {
