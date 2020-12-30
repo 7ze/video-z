907d0951d9ebd27938ffa165d6307015935fc764
The-sad-philosopher <shinudonney@tuta.io>
2020-12-30 15:35:46 +0530

    Add video iframe 📌

--- src/Components/VideoPlayer/VideoPlayer.tsx
+++ src/Components/VideoPlayer/VideoPlayer.tsx
@@ -7,5 +7,18 @@ type Props = {
 };
 
 export const VideoPlayer: React.FC<Props> = ({ video }) => {
-  return <div className="video-player">{video?.snippet.title}</div>;
+  if (video) {
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+    return (
+      <div className="video-player">
+        <div className="ui embed">
+          {<iframe title="video-player" src={videoSrc}></iframe>}
+        </div>
+        <div className="ui segment">
+          <h4 className="header">{video.snippet.title}</h4>
+          <p>{video?.snippet.description}</p>
+        </div>
+      </div>
+    );
+  } else return <div></div>;
 };
