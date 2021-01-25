dcd7e4bac1a8ff1ce3c2b0aa877053c4a305574b
The-sad-philosopher <shinudonney@tuta.io>
2021-01-25 21:03:07 +0530

    🎨 Rewrite App using Hooks

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,64 +1,48 @@
 import * as React from 'react';
-import './App.sass';
 import { Video } from '../VideoInterface';
 import { Searchbar } from '../Searchbar';
 import { VideoList } from '../VideoList';
 import { getVideos } from '../../api';
 import { VideoPlayer } from '../VideoPlayer';
 import { Footer } from '../Footer';
+import './App.sass';
 
-type State = {
-  videos: Video[];
-  selectedVideo: Video | null;
-};
+export const App: React.FC = () => {
+  const [videos, setVideos] = React.useState<Video[]>([]);
+  const [selectedVideo, setSelectedVideo] = React.useState<Video | null>(null);
 
-type Props = {};
+  React.useEffect(() => {
+    (async () => onSearchTermSubmit('Christmas London 4k'))();
+  }, []);
 
-export class App extends React.Component<Props, State> {
-  state: State = {
-    videos: [],
-    selectedVideo: null,
-  };
-
-  onSearchTermSubmit = async (searchTerm: string) => {
+  const onSearchTermSubmit = async (searchTerm: string) => {
     const { videos, error } = await getVideos('/search', searchTerm);
     if (error) console.debug(error);
-    else
-      this.setState({
-        videos,
-        selectedVideo: videos[0],
-      });
+    else {
+      setVideos(videos);
+      setSelectedVideo(videos[0]);
+    }
   };
 
-  onVideoSelect = (selectedVideo: Video) => {
-    this.setState({ selectedVideo });
-  };
+  const onVideoSelect = (selectedVideo: Video) =>
+    setSelectedVideo(selectedVideo);
 
-  componentDidMount() {
-    this.onSearchTermSubmit('4k london christmas');
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <Searchbar onSearchTermSubmit={this.onSearchTermSubmit} />
-        <div className="ui container">
-          <div className="ui stackable grid">
-            <div className="ui row">
-              <div className="eleven wide column">
-                <VideoPlayer video={this.state.selectedVideo} />
-              </div>
-              <div className="five wide column">
-                <VideoList
-                  videos={this.state.videos}
-                  onVideoSelect={this.onVideoSelect}
-                />
-              </div>
+  return (
+    <div className="app">
+      <Searchbar onSearchTermSubmit={onSearchTermSubmit} />
+      <div className="ui container">
+        <div className="ui stackable grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoPlayer video={selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList videos={videos} onVideoSelect={onVideoSelect} />
             </div>
           </div>
         </div>
-        <Footer />
       </div>
-    );
-  }
-}
+      <Footer />
+    </div>
+  );
+};
